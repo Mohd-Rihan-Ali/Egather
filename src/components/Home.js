2bf0a4200a41b483b4d8b26f1245d48c11746ec9
Yousuf <yousufulabrarkhan@gmail.com>
2023-08-04 19:00:53 +0530

    initializing project

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="card text-center my-3">
+  <div className="card-body">
+    <h5 className="card-title">Get Connected: Start or Join a Meeting</h5>
+    <p className="card-text">Grow your business with high-quality video conferencing. Seamlessly connect with clients, colleagues, and partners from anywhere in the world.</p>
+    <a href="#" className="btn btn-primary mx-2">Start New Meeting</a>
+    <a href="#" className="btn btn-primary mx-2">Join Meeting</a>
+  </div>
+</div>
+  )
+}
+
+export default Home
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Hey, iam Login</h1>
+    </div>
+  )
+}
+
+export default Login
