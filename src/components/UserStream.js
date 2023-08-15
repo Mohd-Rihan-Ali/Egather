a773703f05c9d014c31c738d66eb4aa7d12cb595
Yousuf <yousufulabrarkhan@gmail.com>
2023-08-15 15:35:30 +0530

    improving slider

--- src/components/UserStream.js
+++ src/components/UserStream.js
@@ -9,8 +9,29 @@ import UserVideo from './UserVideo';
 
 const UserStreamContainer = () => {
     const settings = {
-        slidesToShow: 3,
+        dots: true,
+        slidesToShow: 4,
         slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1385, // Adjust as needed
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 1032, // Adjust as needed
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 688, // Adjust as needed
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
         prevArrow: <button className="slick-prev">Previous</button>,
         nextArrow: <button className="slick-next">Next</button>,
         // Other slick options
@@ -19,6 +40,9 @@ const UserStreamContainer = () => {
     return (
         <div className="userStreams">
             <Slider {...settings}>
+                <UserVideo/>
+                <UserVideo/>
+                <UserVideo/>
                 <UserVideo/>
                 <UserVideo/>
                 <UserVideo/>
